d79497b2b487323adbfc81aeb49ee9a9a0563122
AbhiRamMaddali <abhiram.maddali1204@gmail.com>
2025-05-22 11:38:44 +0530

    Update bubble sort.py

--- Sorting/programs/bubble sort.py
+++ Sorting/programs/bubble sort.py
@@ -1,9 +1,10 @@
 def bubble(a):
-    for i in range(len(a)):
+    for i in range(len(a)): #: Iterates over each element in the list using index i.
         for j in range(0, len(a) - i - 1):  
-            if a[j] > a[j + 1]:         
-                a[j], a[j + 1] = a[j + 1], a[j]  
+            if a[j] > a[j + 1]:         #Comparison
+                a[j], a[j + 1] = a[j + 1], a[j]   Swap 
 
 a = [5, 3, 7, 6, 8]
 bubble(a)
 print(a)
+# Output [3, 5, 6, 7, 8]
