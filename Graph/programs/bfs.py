bb4d6d1feffb0d6093c2e790e79c918835f0c269
AbhiRamMaddali <abhiram.maddali1204@gmail.com>
2025-05-23 11:26:37 +0530

    Update bfs.py

--- Graph/programs/bfs.py
+++ Graph/programs/bfs.py
@@ -16,3 +16,30 @@ def bfs(graph, root):
 if __name__ == "__main__":
     graph = {0: [1, 2, 3], 1: [0, 2], 2: [0, 1, 4], 3: [0], 4: [2]}
     bfs(graph, 0)  # Output: {0, 1, 2, 3, 4}
+
+
+from collections import deque
+
+def bfs(graph, start):
+    visit = set()
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visit:
+            visit.add(node)
+        if node in graph:  
+            for neighbor in graph[node]:
+                if neighbor not in visit:
+                    queue.append(neighbor)
+    return visit
+
+graph = {
+    0: [1, 2, 3],
+    1: [0, 2],
+    2: [0, 1, 4],
+    3: [0],
+    4: [2]  
+}
+result = bfs(graph, 0)
+print(result)  # Output: {0, 1, 2, 3, 4}
+
