5e50a3c76ca2d9a24a0b6e39fcc3a22f846e78f6
AbhiRamMaddali <abhiram.maddali1204@gmail.com>
2025-05-25 22:55:35 +0530

    Update tree structure .py

--- tree/tree structure .py
+++ tree/tree structure .py
@@ -3,3 +3,17 @@ class Node:
         self.data = data
         self.left = None
         self.right = None
+# Creating nodes
+root = Node('A')
+root.left = Node('B')
+root.right = Node('C')
+root.left.left = Node('D')
+root.left.right = Node('E')
+root.right.right = Node('F')
+'''
+        A
+       / \
+      B   C
+     / \    \
+    D   E    F
+'''
